28da8a6d8de703bab1b0e47b6fcfd73b63690fb7
9631044 <9631044@ceit.local>
2019-09-29 05:18:41 +0330

    refactor: removing useless files

--- simulator/indicator/serializers.py
+++ simulator/indicator/serializers.py
@@ -1,7 +1,6 @@
-from rest_framework import serializers
+# from rest_framework import serializers
 
-
-def StockSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Stock
-        # fields = ('url', 'username', 'email', 'groups')
+# def StockSerializer(serializers.Serializer):
+#     class Meta:
+#         model = Stock
+#         # fields = ('url', 'username', 'email', 'groups')
--- simulator/indicator/tasks.py
+++ simulator/indicator/tasks.py
@@ -1,53 +1,53 @@
 # from background_task import background
 from indicator.celery import app
-from indicator.models import Indicator, Stock, Record
+# from indicator.models import Indicator, Stock, Record
 from datetime import datetime
 # from indicator.views import update
 from . import views
 
 
-@app.task
-def update_indicators():
-    print('time', datetime.now())
-    indicators = Indicator.objects.all()
-    print('here')
-    for indicator in indicators:
-        indicator.update_time_control()
-        print('updated')
+# @app.task
+# def update_indicators():
+#     print('time', datetime.now())
+#     indicators = Indicator.objects.all()
+#     print('here')
+#     for indicator in indicators:
+#         indicator.update_time_control()
+#         print('updated')
 
 
-@app.task
-def update_indicator(name):
-	indicator = Indicator.objects.all().get(name=name)
-	indicator.update_time_control()
+# @app.task
+# def update_indicator(name):
+# 	indicator = Indicator.objects.all().get(name=name)
+# 	indicator.update_time_control()
 
 
-@app.task
-def update_records():
-    Record.objects.filter(Ticker='').delete()
-    print('day records deleted', Record.objects.filter(Ticker=''))
-    views.update()
+# @app.task
+# def update_records():
+#     Record.objects.filter(Ticker='').delete()
+#     print('day records deleted', Record.objects.filter(Ticker=''))
+#     views.update()
 
 
-@app.task
-def update_today():
-    views.update_stocks_today()
-    stocks = Stock.objects.all()
-    today = Indicator.datetime_to_dateint(datetime.now())
-    for stock in stocks:
-        new_record = Record(
-            stock=stock,
-            date=today,
-            first=stock.avalin,
-            high=stock.baze_rooz_ziad,
-            low=stock.baze_rooz_kam,
-            close=stock.payani,
-            value=stock.arzesh_moamelat,
-            vol=stock.hajm_moamelat,
-            openint=stock.tedad_moamelat,
-            per='D',
-            openp=stock.dirooz,
-            last=stock.akharin_moamele
-            )
-        print('new', new_record, today)
-        new_record.save()
+# @app.task
+# def update_today():
+#     views.update_stocks_today()
+#     stocks = Stock.objects.all()
+#     today = Indicator.datetime_to_dateint(datetime.now())
+#     for stock in stocks:
+#         new_record = Record(
+#             stock=stock,
+#             date=today,
+#             first=stock.avalin,
+#             high=stock.baze_rooz_ziad,
+#             low=stock.baze_rooz_kam,
+#             close=stock.payani,
+#             value=stock.arzesh_moamelat,
+#             vol=stock.hajm_moamelat,
+#             openint=stock.tedad_moamelat,
+#             per='D',
+#             openp=stock.dirooz,
+#             last=stock.akharin_moamele
+#             )
+#         print('new', new_record, today)
+#         new_record.save()
